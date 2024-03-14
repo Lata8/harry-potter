d6964f196dfa0f78d2d3d7f35ce0d5e78d5207cc
Lata8 <eliasibarra565@gmail.com>
2024-03-14 15:14:34 -0300

    QUIZ

---
+++ funciones.js
@@ -0,0 +1,27 @@
+function calculateScore() {
+    const answers = {
+      question1: 'b',
+      question2: 'c',
+      question3: 'a',
+      question4: 'c',
+      question5:'a',
+      question6:'b',
+      question7:'c',
+      question8:'b',
+      question9:'b'
+    };
+
+    let score = 0;
+
+    for (const question in answers) {
+      const selectedOption = document.querySelector(`input[name=${question}]:checked`);
+      if (selectedOption) {
+        if (selectedOption.value === answers[question]) {
+          score++;
+        }
+      }
+    }
+
+    const resultContainer = document.getElementById('result');
+    resultContainer.innerHTML = `ACERTASTE ${score} DE ${Object.keys(answers).length}`;
+}
